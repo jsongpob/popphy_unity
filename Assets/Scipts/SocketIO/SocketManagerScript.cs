4e90daae689cc8fb7c418a4e2cade4a9e1bbf2cb
Songpob <jsongpob@gmail.com>
2024-02-15 11:59:53 +0700

    Add ProGIF package & SocketIO

--- Assets/Scipts/SocketIO/SocketManagerScript.cs
+++ Assets/Scipts/SocketIO/SocketManagerScript.cs
@@ -1,14 +1,22 @@
 using System.Collections;
+using System;
+using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
 using BestHTTP.SocketIO3;
-using System;
+using BestHTTP.SocketIO.Events;
+using static UnityEngine.Rendering.DebugUI;
+using static UnityEditor.Progress;
 using BestHTTP.SocketIO3.Events;
 
 public class SocketManagerScript : MonoBehaviour
 {
     private SocketManager manager;
-    public String SocketValue;
+
+    private void Awake()
+    {
+
+    }
 
     void Start()
     {
@@ -16,40 +24,34 @@ public class SocketManagerScript : MonoBehaviour
 
         SocketOptions options = new SocketOptions();
         options.AutoConnect = false;
-
         this.manager = new SocketManager(new Uri("http://localhost:3000"), options);
 
-        manager.Socket.On<ConnectResponse>(SocketIOEventTypes.Connect, onConnected);
+        manager.Socket.On<ConnectResponse>(SocketIOEventTypes.Connect, OnConnected);
         manager.Socket.On<Error>(SocketIOEventTypes.Error, OnError);
 
         this.manager.Open();
 
-        manager.Socket.On<int>("test", TestSocket);
+        manager.Socket.On<string>("test", OnMethod);
+        manager.Socket.Emit("channel_platfrom", "msg 1");
     }
 
-    void onConnected(ConnectResponse resp)
+    void OnConnected(ConnectResponse resp)
     {
-        Debug.Log("Connected");
+        Debug.Log("Connected!");
     }
-
     void OnError(Error error)
     {
         Debug.Log($"An error occured: {error}");
     }
-
     void OnDestroy()
     {
         this.manager?.Close();
         this.manager = null;
     }
 
-    private void TestSocket(int value)
+    void OnMethod(string data)
     {
-        Debug.Log(value);
-        if (value == 1)
-        {
-            Debug.Log("Hello Socket!");
-        }
+        Debug.Log("OnMethod!" + data);
     }
 
 }
