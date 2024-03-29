31e67d29eb9601ea4e7394273af09cf7c7743bfa
Songpob <jsongpob@gmail.com>
2024-03-30 01:31:43 +0700

    1.0.2
    add credit to picture

--- Assets/Scipts/ProGifRecording_Popphy.cs
+++ Assets/Scipts/ProGifRecording_Popphy.cs
@@ -41,7 +41,7 @@ public class ProGifRecording_Popphy : MonoBehaviour
         ProGifManager gifMgr = ProGifManager.Instance;
 
         //Make some changes to the record settings, you can let it auto aspect with screen size.. 
-        gifMgr.SetRecordSettings(true, 360, 360, 5, 4, 0, 25); //OPTIMIZE VER
+        gifMgr.SetRecordSettings(true, 360, 360, 5, 4, 0, 35); //OPTIMIZE VER
         //gifMgr.SetRecordSettings(true, 720, 1280, 5, 50, 0, 80);
 
         //Or give an aspect ratio for cropping gif frames just before encoding
